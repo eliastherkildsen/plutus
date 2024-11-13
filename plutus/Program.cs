eef8486dce07cbb01a73af85b77c2d294791c3ed
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-11-13 17:53:48 +0100

    feat(API): program
    Implement validation for the API.

--- plutus/Program.cs
+++ plutus/Program.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
 using plutus.Entity;
 using plutus.IRepository;
 using plutus.Repository;
@@ -47,13 +49,75 @@ public class Program
         app.MapGet("/parcels/", async (IParcelRepository parcelRepository) => await parcelRepository.GetAll());
         
         
-        app.MapPost("/parcel/", async (Parcel parcel, IParcelRepository parcelRepository) =>
+        app.MapPost("/parcel/", async ([FromBody] Parcel parcel, IParcelRepository parcelRepository) =>
         {
-            await parcelRepository.Add(parcel);
+            
+            // setting the last updated time to now. 
+            parcel.LastUpdate = DateTime.Now;
+            
+            try
+            {
+                await parcelRepository.Add(parcel);
+            }
+
+            catch (InvalidDataException ex)
+            {
+                return Results.BadRequest(new { Error = "InvalidData", Message = ex.Message });
+            }
+            catch (DuplicateNameException ex)
+            {
+                return Results.BadRequest(new { Error = "DuplicateName", Message = ex.Message });
+            }
+            catch (ArgumentException ex)
+            {
+                return Results.BadRequest(new { Error = "ArgumentException", Message = ex.Message });
+            }
+
+            // we can not ensure that the err messages is safe to displayed, therefor we don't. 
+            catch (Exception err)
+            {
+                return Results.Problem(statusCode: 500, title: "An unexpected error occurred.");
+            }
+            
+            return Results.Ok(parcel);
+            
         });
-        
-        app.MapPut("/parcel/", async (string id, Parcel parcel, IParcelRepository parcelRepository) => 
-            await parcelRepository.Update(id, parcel));
+
+        app.MapPut("/parcel/", async (string id, Parcel parcel, IParcelRepository parcelRepository) =>
+        {
+            
+            // setting the last updated time to now, and id to the id passed in the header. This means that the id
+            // can not be updated. 
+            parcel.LastUpdate = DateTime.Now;
+            parcel.Id = id;
+
+            try
+            {
+                await parcelRepository.Update(id, parcel);
+            }
+            
+            catch (InvalidDataException ex)
+            {
+                return Results.BadRequest(new { Error = "InvalidData", Message = ex.Message });
+            }
+            catch (DuplicateNameException ex)
+            {
+                return Results.BadRequest(new { Error = "DuplicateName", Message = ex.Message });
+            }
+            catch (ArgumentException ex)
+            {
+                return Results.BadRequest(new { Error = "ArgumentException", Message = ex.Message });
+            }
+
+            // we can not ensure that the err messages is safe to displayed, therefor we don't. 
+            catch (Exception err)
+            {
+                return Results.Problem(statusCode: 500, title: "An unexpected error occurred.");
+            }
+            
+            return Results.Ok(parcel);
+            
+        });
         
         app.Run();
     }    
