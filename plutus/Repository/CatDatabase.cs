de8c0c19a695393969182f3539404e52e3c38c85
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-11-13 09:13:41 +0100

    inital commit

---
+++ plutus/Repository/CatDatabase.cs
@@ -0,0 +1,11 @@
+﻿namespace plutus.Repository;
+
+public class CatDatabase
+{
+    public string? ConnectionString { get; set; }  
+    public string? DatabaseName { get; set; }       
+    public string? CollectionName { get; set; }    
+}
+
+
+
